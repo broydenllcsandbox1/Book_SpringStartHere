46b941ee2c819436a8c74d7ebd81295329b97ddf
Michael Morrison <broydenllc.sandbox1@gmail.com>
2023-01-14 23:01:42 -0600

    completing example plus adding codes samples downloaded from manning press site

--- chapters/2/2.1.1/src/main/java/config/ProjectConfig.java
+++ chapters/2/2.1.1/src/main/java/config/ProjectConfig.java
@@ -7,9 +7,23 @@ import main.Parrot;
 @Configuration
 public class ProjectConfig {
     @Bean
-    Parrot parrot() {
+    Parrot parrot1() {
         var p = new Parrot();
         p.setName("Koko");
+        return p;
+    }
+
+    @Bean
+    Parrot parrot2() {
+        var p = new Parrot();
+        p.setName("Miki");
+        return p;
+    }
+
+    @Bean
+    Parrot parrot3() {
+        var p = new Parrot();
+        p.setName("Riki");
         return p;
     }
 
